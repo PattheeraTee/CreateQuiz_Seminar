ce1aceb43ad72fe0d7d7a40548f4fb5fb0472748
PattheeraTee <pattheera.mwny@gmail.com>
2024-10-15 03:52:53 +0700

    add

--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -1,30 +1,27 @@
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRobot, faFileImport, faGlobe,faSimCard} from '@fortawesome/free-solid-svg-icons';
 
 export default function Sidebar() {
   return (
     <aside className="w-64 h-screen bg-gray-100 p-4">
-      {/* Quiz Section */}
+
       <div className="flex items-center space-x-2 p-4 mb-4 bg-gray-200 rounded-lg">
-        {/* <FaGlobe className="text-2xl text-black" /> */}
+        <FontAwesomeIcon icon={faGlobe} className="w-6 h-6 text-black"/>
         <span className="text-xl font-semibold text-black">ควิซของฉัน</span>
       </div>
 
-      {/* Game Section */}
-      <div className="flex items-center space-x-2 p-4 mb-4 bg-gray-200 rounded-lg">
-        {/* <FaGamepad className="text-2xl text-black" /> */}
-        <span className="text-xl font-semibold text-black">เกมเพลต</span>
+      <div className="flex items-center space-x-2 p-4 mb-4 bg-gray-200 rounded-lg ">
+        <FontAwesomeIcon icon={faSimCard} className="w-6 h-6 text-black"/>
+        <span className="text-xl font-semibold text-black">เทมเพลต</span>
       </div>
 
-      {/* AI Section */}
-      <div className="flex items-center space-x-2 p-4 mb-4 bg-gray-200 rounded-lg">
-        {/* <FaRobot className="text-2xl text-black" /> */}
+      <div className="flex items-center space-x-2 p-4 mb-4 bg-gray-200 rounded-lg text-black">
+        <FontAwesomeIcon icon={faRobot} className="w-6 h-6 text-black" />
         <span className="text-xl font-semibold text-black">AI</span>
       </div>
 
-      {/* Import Section */}
-      <div className="flex items-center space-x-2 p-4 bg-purple-200 rounded-lg">
-        {/* <FaFileImport className="text-2xl text-purple-600" /> */}
+      <div className="flex items-center space-x-2 p-4 mb-4 bg-purple-200 rounded-lg">
+        <FontAwesomeIcon icon={faFileImport} className="w-6 h-6 text-purple-600" />
         <span className="text-xl font-semibold text-purple-600">นำเข้าเอกสาร</span>
       </div>
     </aside>
